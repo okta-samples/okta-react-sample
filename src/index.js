0fdc8cc4a12933783d953608505a41a9e33dcb3f
Matt Raible <matt.raible@okta.com>
2021-03-15 11:42:58 -0600

    Polishing

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './polyfills';
 import App from './App';
