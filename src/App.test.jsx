cd39a8e8b3570b896b098f971d1b73a331e9dff0
Matt Raible <matt.raible@okta.com>
2022-04-26 09:14:06 -0600

    Revert to Auth JS 6.1.0 to fix tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,41 +3,8 @@ import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
 import { act } from 'react-dom/test-utils';
 import App from './App';
-import { OktaAuth } from '@okta/okta-auth-js';
 
 let container;
-
-jest.mock('broadcast-channel', () => {
-  const actual = jest.requireActual('broadcast-channel');
-  class FakeBroadcastChannel {
-    close() {}
-  }
-  return {
-    createLeaderElection: actual.createLeaderElection,
-    BroadcastChannel: FakeBroadcastChannel
-  };
-});
-
-const mocked = {
-  broadcastChannel: require('broadcast-channel'),
-};
-
-function createAuth(options) {
-  options = options || {};
-  options.tokenManager = options.tokenManager || {};
-  options.services = options.services || {};
-  return new OktaAuth({
-    issuer: 'https://auth-js-test.okta.com',
-    clientId: 'NPSfOkH5eZrTy8PMDlvx',
-    redirectUri: 'https://example.com/redirect',
-    tokenManager: {
-      syncStorage: options.tokenManager.syncStorage || false,
-      autoRenew: options.tokenManager.autoRenew || false,
-      autoRemove: options.tokenManager.autoRemove || false,
-    },
-    services: options.services
-  });
-}
 
 beforeEach(() => {
   container = document.createElement('div');
@@ -50,16 +17,10 @@ afterEach(() => {
 });
 
 it('renders title link', async () => {
-  const options = { tokenManager: { syncStorage: false, autoRenew: true } };
-  const client = createAuth(options);
-  client.serviceManager.start();
-
   await act(async () => {
     ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, container);
   });
 
   const linkElement = container.querySelector('a');
   expect(linkElement.textContent.trim()).toBe('Okta-React Sample Project');
-
-  client.serviceManager.stop();
 });
