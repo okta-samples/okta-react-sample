36badd8350b90601eedaedaaf08743f495794ad6
Matt Raible <matt.raible@okta.com>
2022-09-05 13:45:18 -0600

    Fix stack traces in tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,6 @@
 // broadcast-channel should not detect node environment
 // https://github.com/pubkey/broadcast-channel/blob/master/src/util.js#L61
 process[Symbol.toStringTag] = 'Process';
+
+// https://github.com/testing-library/react-testing-library/issues/1061#issuecomment-1117450890
+global.IS_REACT_ACT_ENVIRONMENT = true
