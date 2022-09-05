4949101b4f32a469b2f652cc218ff5d6a464a7f8
Matt Raible <matt.raible@okta.com>
2022-09-05 13:48:56 -0600

    Polishing

--- jest.setup.js
+++ jest.setup.js
@@ -2,5 +2,6 @@
 // https://github.com/pubkey/broadcast-channel/blob/master/src/util.js#L61
 process[Symbol.toStringTag] = 'Process';
 
+// solves stacktrace when running tests
 // https://github.com/testing-library/react-testing-library/issues/1061#issuecomment-1117450890
 global.IS_REACT_ACT_ENVIRONMENT = true
