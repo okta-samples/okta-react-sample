b3ba332480b8eaf34edfee2b6d2a7e5b38606b98
Alisa <7586237+alisaduncan@users.noreply.github.com>
2024-11-25 14:34:02 -0800

    update deps

--- src/config.js
+++ src/config.js
@@ -1,17 +1,11 @@
-const CLIENT_ID = process.env.CLIENT_ID || '{clientId}';
-const ISSUER = process.env.ISSUER || 'https://{yourOktaDomain}/oauth2/default';
-const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
-const REDIRECT_URI = `${window.location.origin}/login/callback`;
 
 // eslint-disable-next-line
 export default {
   oidc: {
     clientId: CLIENT_ID,
     issuer: ISSUER,
-    redirectUri: REDIRECT_URI,
+    redirectUri: window.location.origin + '/login/callback',
     scopes: ['openid', 'profile', 'email', 'offline_access'],
-    pkce: true,
-    disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
   },
   resourceServer: {
     messagesUrl: 'http://localhost:8000/api/messages',
--- src/index.jsx
+++ src/index.jsx
@@ -15,10 +15,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
-import './polyfills';
 import App from './App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -26,4 +24,3 @@ root.render(
   <BrowserRouter>
     <App/>
   </BrowserRouter>);
-registerServiceWorker();
